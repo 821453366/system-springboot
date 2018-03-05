7244641b8d72c4fa002ef793c07ae769659d6548
821453366 <821453366@qq.com>
2018-03-05 19:59:32 +0800

    创建一个spring-boot项目

---
+++ src/main/java/com/example/systemspringboot/SystemSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.systemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemSpringbootApplication.class, args);
+	}
+}
